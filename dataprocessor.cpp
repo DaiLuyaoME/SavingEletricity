f0440b221c80910e5d7cf820880ed3ec8c96b5b1
luoyang <ly223038@163.com>
2016-08-05 16:56:36 +0800

    20160804-问题与缺陷，见README.md

--- dataprocessor.cpp
+++ dataprocessor.cpp
@@ -1,33 +1,99 @@
 #include "dataprocessor.h"
 
+#define REALTIMEDATABUFFERCOUNT 3600//realTimeDataBuffer 数据个数
+#define GETDATATIMEDEFAULT      1//默认读取数据间隔 单位：second
+#define SAVEDATATIMEDEFALT      10//默认存储数据间隔 单位：second
+#define MONITERTIMEDEFALT       600//默认监控计算间隔 单位：second
+#define ERROR                   -1//报错回传参数
+
 DataProcessor::DataProcessor(QObject *parent) : QObject(parent)
 {
 	/*实例初始化*/
-	/*有关串口的实例都要注意串口号的占用与释放*/
+    /*！！！有关串口的实例都要注意串口号的占用与释放*/
+    database = new Database(this);//数据库实例
 	ammeter = new Ammeter(this);//电表实例
 	regulator  = new Regulator(this);//下位机实例
 	getDataTimer = new QTimer(this);//读取数据实例 1s读取一次数据
 	saveDataTimer = new QTimer(this);//存储数据实例 10s存储一次数据
 	moniterTimer = new QTimer(this);//电表监控实例 计算功率
+    realTimeDataBuffer = new QList<DataPoint>;//数据组实例 ***！！？ 数据个数确定***
 
-	realTimeDataBuffer???
+
+    /*参数初始化*/
+    OldAveragePower       = 0.0;
+    OldTotalPower         = 0.0;
+    AveragePower          = 0.0;
+    TotalPower            = 0.0;
+
+    GetDataTimeInterval   = GETDATATIMEDEFAULT;
+    SaveDataTimeInterval  = SAVEDATATIMEDEFALT;
+    MoniterTimeInterval   = MONITERTIMEDEFALT;
+
+
+
 	/*1s读取数据*/
 	QObject::connect(getDataTimer,SIGNAL(timeout()),this,SLOT(getData()));
-	getDataTimer->start(1000);//1s一次
+    getDataTimer->start(GetDataTimeInterval*1000);//默认1s一次
+    /*10S存储数据*/
+    QObject::connect(saveDataTimer,SIGNAL(timeout()),this,SLOT(saveData());
+    saveDataTimer->start(SaveDataTimeInterval*1000);//默认10s一次
+    /*10min监控一次*/
+    QObject::connect(moniterTimer,SIGNAL(timeout()),this,SLOT(getAveragePower(MONITERTIME));//计算前10min平均功率
+    QObject::connect(moniterTimer,SIGNAL(timeout()),this,SLOT(getMinPower(MONITERTIME));//计算前10min最小功率
+    moniterTimer->start(MoniterTimeInterval);//默认600s一次
 
-	/*存储数据*/
+
+
 
 
 }
 
+/*
+*通过电表ammeter读取数据
+*
+*/
 void DataProcessor::getData()
 {
+    int Isread;
+    DataPoint tempdata;
+    Isread = ammeter->readLatestData();
+    if(Isread == ERROR)
+    {
+        emit ammeterError();//发送电表错误消息
+    }
+    else
+    {
+        tempdata = ammeter->getData();//读取数据
+        if(realTimeDataBuffer.size()>=REALTIMEDATABUFFERCOUNT)//保留3600个数据
+        {
+            realTimeDataBuffer.pop_front();
+            realTimeDataBuffer.push_back(tempdata);
+        }
+        else
+        {
+            realTimeDataBufferl.push_back(tempdata);//队列处理
+        }
+    }
 
 }
 
+/*
+*向数据库中写入数据
+*/
 void DataProcessor::saveData()
 {
-
+    bool Isnodata;
+    Isnodata = realTimeDataBuffer.isEmpty();
+    if(Isnodata)//没有数据存储
+    {
+        return 0;
+    }
+    else
+    {
+        /*!!?如果此处的last()函数返回的是指针,不用加星号“*”。*/
+        /*!!!database 类中没有报错机制*/
+        database->saveData(realTimeDataBuffer.last());//存储当前读取的最新数据
+    }
 }
 
 void DataProcessor::startMonitor()
@@ -40,22 +106,37 @@ void DataProcessor::stopMonitor()
 
 }
 
+/*
+ * 根据绘图所需点的个数来
+*/
 QList<DataPoint> DataProcessor::dataSlicer(QTime begin, QTime end)
 {
 
 }
 
-void DataProcessor::setTimeInterval()
+/*
+*设置时间间隔 单位：S
+* timetype:
+* “1”：GetDataTime 采样数据间隔
+* ”2“: SaveDataTime 存储数据间隔
+* ”3“: MoniterTime  监控时间间隔
+*/
+void DataProcessor::setTimeInterval(int interval, int timettype)
 {
 
 }
 
-void DataProcessor::getAveragePower(int timeLength)
+/*
+*计算当前10分钟内的平均功率，600个数据
+*返回一个浮点功率
+*/
+float DataProcessor::getAveragePower(int timeLength)
 {
 
 }
 
-void DataProcessor::getMinPower(int timeLength)
+
+float DataProcessor::getMinPower(int timeLength)
 {
 
 }
--- dataprocessor.h
+++ dataprocessor.h
@@ -15,6 +15,7 @@ class DataProcessor : public QObject
     Q_OBJECT
 public:
     explicit DataProcessor(QObject *parent = 0);
+    ~DataProcessor();
 
 signals:
     void ammeterError();
@@ -27,9 +28,19 @@ public slots:
     void startMonitor();
     void stopMonitor();
     QList<DataPoint> dataSlicer(QTime begin,QTime end);
-    void setTimeInterval();
-    void getAveragePower(int timeLength);//second
-    void getMinPower(int timeLength);//second
+    void setTimeInterval(int interval,int timettype);//设置不同时间间隔
+    float getAveragePower(int timeLength);//second
+    float getMinPower(int timeLength);//second
+
+    float OldAveragePower;//上一个监控点的平均功率
+    float OldTotalPower;//上一个监控点间隔时间内的总功率
+
+    //单位：Second
+    int   GetDataTimeInterval;//采样数据间隔
+    int   SaveDataTimeInterval;//存储数据间隔
+    int   MoniterTimeInterval;//监控数据间隔
+
+
 
 private:
     Database *database;
@@ -39,6 +50,12 @@ private:
     QTimer *saveDataTimer;//默认每10秒发动一次
     QTimer *monitorTimer;//每10分钟发动一次
     QList<DataPoint> realTimeDataBuffer;//1point/1s,length=3600
+
+    float AveragePower;//当前监控点的平均功率
+    float TotalPower;//当前时间间隔内的总功率
+    int MinMomentPower;//瞬时功率最小值
+
+    void moniter(int timelength);//启动监控
 
 };
 
