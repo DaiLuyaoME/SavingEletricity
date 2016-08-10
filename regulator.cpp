eb332e5bf2a70fd11f632041bd3b449387f8b944
dailuyao <dailuyaome@foxmail.com>
2016-08-10 11:20:33 +0800

    （1）修改和补充了下位机类（regulator）的部分接口；（2）修改了电表类（ammeter）类中的部分错误

--- regulator.cpp
+++ regulator.cpp
@@ -5,17 +5,47 @@ Regulator::Regulator(QObject *parent) : QObject(parent)
 
 }
 
-void Regulator::sendVotageAtMinPower(datatype voltage)
-{
-
-}
-
 void Regulator::beginRegulate()
 {
 
 }
 
+//具体的串口产生需要根据实际情况进行调整
+void Regulator::openPort()
+{
+    regulatorPort=new QSerialPort(this);
+    foreach (const QSerialPortInfo &info,QSerialPortInfo::availablePorts())
+    {
+        QString portDescription=info.description();
+        if(portDescription.contains(QString("USB")))
+        {
+            regulatorPort->setPort(info);
+        }
+
+    }
+
+    regulatorPort->setBaudRate(QSerialPort::Baud9600);
+    regulatorPort->setDataBits(QSerialPort::Data8);
+    regulatorPort->setParity(QSerialPort::EvenParity);
+    regulatorPort->setStopBits(QSerialPort::OneStop);
+    regulatorPort->setFlowControl(QSerialPort::NoFlowControl);
+    regulatorPort->open(QIODevice::ReadWrite);
+    connect(regulatorPort,&QSerialPort::readyRead,this,&Regulator::parseData);
+//    connect(regulatorPort,&QSerialPort::bytesWritten,this,&Regulator::startTimeoutTimer);
+
+}
+
 void Regulator::ack()
+{
+
+}
+
+void Regulator::sendVoltageToRegulator(DataPoint &data)
+{
+
+}
+
+void Regulator::parseData()
 {
 
 }
